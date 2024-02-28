ba186c10d30233bcc313e6ef3ef47ebf4a393711
Emmanuel Hernandez <e805177@gmail.com>
2024-02-28 03:57:30 -0600

    Projects data were added

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,41 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'TAILVERSE',
+    description: `Get started with a large variety of Tailwind CSS templates, tailwind translator and css to tailwind`,
+    imgSrc: '/static/images/team.svg',
+    href: 'https://tailverse.tech',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'BYSMAX',
+    description: `Have a project in mind?
+      We d love to talk about how we can help you.`,
+    imgSrc: '/static/images/creative.svg',
+    href: 'https://bysmax.com',
+  },
+  {
+    title: 'VETADMIN',
+    description: `VetAdmin Pro is a veterinary patient management software crafted with HTML, CSS, and JavaScript. It employs IndexDB as a local database for storing patient information.`,
+    imgSrc: '/static/images/vetadmin.png',
+    href: 'https://vetadmin.emmanuelh.dev/',
+  },
+  {
+    title: 'LA CAFETERIA | STARTER',
+    description: `Discover our restaurant template crafted with Tailwind CSS and Gatsby. Browsing our menu is now a breeze, making the dining experience hassle-free and enjoyable.`,
+    imgSrc: '/static/images/gallery.png',
+    href: 'https://la-cafeteria-with-tailwindcss.vercel.app/',
+  },
+  {
+    title: 'Drinks Browser',
+    description: `Search and make coctels and drinks with ai and more.`,
+    imgSrc: '/static/images/drink.jpg',
+    href: 'https://drinksbrowser.vercel.app',
   },
 ]
 
