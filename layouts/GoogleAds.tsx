2f917b31821d6ce1ae9a1c9fa897cec00d7a494a
Emmanuel Hernandez <e805177@gmail.com>
2024-06-03 01:11:27 -0600

    Update GoogleAds.tsx

--- layouts/GoogleAds.tsx
+++ layouts/GoogleAds.tsx
@@ -1,25 +1,37 @@
-'use client'
 import React, { useEffect } from 'react'
 
-export default function GoogleAds() {
+const AdComponent: React.FC = () => {
   useEffect(() => {
+    // Crear un nuevo script
+    const script = document.createElement('script')
+
+    // Configurar los atributos del script
+    script.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+    script.async = true
+
+    // Agregar el script al body del documento
+    document.body.appendChild(script)
+
+    // Ejecutar el script de anuncios una vez que el componente se monta
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const adsbygoogle = (window as any).adsbygoogle
-    if (adsbygoogle && adsbygoogle.push) {
-      adsbygoogle.push({})
+    ;((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({})
+
+    // Limpiar el script al desmontar el componente
+    return () => {
+      document.body.removeChild(script)
     }
-  }, [])
+  }, []) // El array vacío como segundo argumento asegura que este efecto solo se ejecute una vez después del montaje
 
   return (
-    <div>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'inline-block', width: 220, height: 728 }}
-        data-ad-client="ca-pub-3646138644530578"
-        data-ad-slot="6731750998"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-3646138644530578"
+      data-ad-slot="6731750998"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   )
 }
+
+export default AdComponent
